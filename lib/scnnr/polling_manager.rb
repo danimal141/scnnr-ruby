fb1216b9aecd444244603f4b733978074524bd95
danimal141 <hideaki.ishii1204@gmail.com>
2017-10-18 12:30:37 +0900

    Add sample implementation

--- lib/scnnr/polling_manager.rb
+++ lib/scnnr/polling_manager.rb
@@ -14,16 +14,34 @@ module Scnnr
       end
     end
 
-    def remain_timeout?
-      self.timeout.positive?
-    end
-
     def polling(client, recognition_id, options = {})
       loop do
         timeout = [self.timeout, MAX_TIMEOUT].min
         self.timeout -= timeout
         recognition = client.fetch(recognition_id, options.merge(timeout: timeout, polling: false))
-        break recognition if recognition.finished? || !self.remain_timeout?
+        raise TimeoutError.new('recognition timed out', recognition) if timed_out?(recognition)
+        break recognition if recognition.finished? || !remain_timeout?
+      end
+    end
+
+    private
+
+    def remain_timeout?
+      self.timeout.positive?
+    end
+
+    def timed_out?(recognition)
+      recognition.queued? && !remain_timeout?
+    end
+
+    class << self
+      def start(client, options, &block)
+        timeout = options.delete(:timeout)
+        used_timeout = [timeout, MAX_TIMEOUT].min
+        recognition = block.call(options.merge(timeout: used_timeout))
+        extra_timeout = timeout - used_timeout
+        return self.new(extra_timeout).polling(client, recognition.id, options) if recognition.queued? && extra_timeout.positive?
+        recognition
       end
     end
   end
